98a73edf5448cb6e47947626b7e8a57c93714e9f
Michael <mblank@bawue.de>
2015-01-03 16:43:30 +0100

    Create sparkconfig.js

---
+++ sparkconfig.js
@@ -0,0 +1,16 @@
+// sparkconfig.js
+// (c) Michael Blank - 2014-2015
+// license: GPLv3
+//
+// example configuration file for the lanbahn_module.js
+// replace token and url with your own access_token and
+// spark-server URL
+
+/*jslint node: true */
+
+module.exports = {
+    // this acces_token for accessing the spark-server
+    "access_token": "00000000000d2cbed6ded412de8fe30000000000",
+    // the spark servers URI, including port
+    "server_url": "http://192.168.178.1:8080"
+};
